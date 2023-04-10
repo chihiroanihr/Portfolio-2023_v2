f60805a441ba8583345ef8c6de8837c7bf1f25cd
Rhina Kim <chihiroanihr@gmail.com>
2023-04-10 13:06:11 -0400

    colors.js constants added (not used yet)
    Might be useful later.

---
+++ src/constants/colors.js
@@ -0,0 +1,23 @@
+export const bgColors = {
+  "coffee-100": "bg-coffee-100",
+  "coffee-300": "bg-coffee-300",
+  "coffee-400": "bg-coffee-400",
+  "coffee-600": "bg-coffee-600",
+  "coffee-800": "bg-coffee-800",
+};
+
+export const textColors = {
+  "coffee-100": "text-coffee-100",
+  "coffee-300": "text-coffee-300",
+  "coffee-400": "text-coffee-400",
+  "coffee-600": "text-coffee-600",
+  "coffee-800": "text-coffee-800",
+};
+
+export const menuBorderColors = {
+  "coffee-100": "after:border-coffee-100",
+  "coffee-300": "after:border-coffee-300",
+  "coffee-400": "after:border-coffee-400",
+  "coffee-600": "after:border-coffee-600",
+  "coffee-800": "after:border-coffee-800",
+};
