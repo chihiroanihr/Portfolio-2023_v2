ad19fbe357353882a1cc78145ee55455747dbe58
Rhina Kim <chihiroanihr@gmail.com>
2023-04-15 23:18:18 -0400

    Update useBodyScrollLocks.js
    - import from "body-scroll-lock" -> "body-scroll-lock-upgrade"
    - some change in variable names
    - useEffect() -> useLayoutEffect()

--- src/utils/useBodyScrollLocks.js
+++ src/utils/useBodyScrollLocks.js
@@ -1,31 +1,31 @@
 // Reference:
 // https://note.com/101design/n/n164943bec0d4
 
-import { useLayoutEffect, useRef } from "react";
+import { useLayoutEffect } from "react";
 import {
   clearAllBodyScrollLocks,
   disableBodyScroll,
   enableBodyScroll,
-} from "body-scroll-lock";
+} from "body-scroll-lock-upgrade";
 
-const useBodyScrollLock = ({ isActive, target }) => {
-  const targetRef = useRef(null);
-
-  useEffect(() => {
-    targetRef.current = target.current;
-
-    if (!targetRef.current) {
+const useBodyScrollLock = ({ isScrollLocked, scrollLockTargetRef }) => {
+  useLayoutEffect(() => {
+    if (!scrollLockTargetRef.current) {
       return;
     }
 
-    if (isActive) {
-      disableBodyScroll(targetRef.current);
-    } else {
-      enableBodyScroll(targetRef.current);
+    // Disable scroll other than target
+    if (isScrollLocked) {
+      disableBodyScroll(scrollLockTargetRef.current);
+    }
+    // Enable scroll other than target
+    else {
+      enableBodyScroll(scrollLockTargetRef.current);
     }
 
+    // Clear all scroll locks when unmounted
     return () => clearAllBodyScrollLocks();
-  }, [isActive, target.current]);
+  }, [isScrollLocked, scrollLockTargetRef.current]);
 
   return null;
 };
