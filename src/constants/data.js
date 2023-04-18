51a4d91aa8c078d8b134259be2ee15b877b914ec
Rhina Kim <chihiroanihr@gmail.com>
2023-04-18 14:08:00 -0400

    Update data.js - alt name changed

--- src/constants/data.js
+++ src/constants/data.js
@@ -6,7 +6,7 @@ import coffeeBgImg3 from "../assets/images/coffee-bg-3.jpg";
 // From Underneath To Top
 export const homeImageCards = [
   {
-    id: "coffee-img-3",
+    id: "coffee-bg-img-3",
     img: coffeeBgImg3,
     style: {
       position: "top-[45%] right-[50%]",
@@ -15,7 +15,7 @@ export const homeImageCards = [
     },
   },
   {
-    id: "coffee-img-2",
+    id: "coffee-bg-img-2",
     img: coffeeBgImg2,
     style: {
       position: "top-1/2 xs:right-[30%] right-[34%]",
@@ -24,7 +24,7 @@ export const homeImageCards = [
     },
   },
   {
-    id: "coffee-img-1",
+    id: "coffee-bg-img-1",
     img: coffeeBgImg1,
     style: {
       position: "top-[60%] xs:right-[5%] right-[16%]",
