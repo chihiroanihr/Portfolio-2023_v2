fba00a886107c489234b4ded533f57b602b1f380
Rhina Kim <chihiroanihr@gmail.com>
2023-04-20 03:33:41 -0400

    Coffee2.jsx added

---
+++ src/components/Coffee2.jsx
@@ -0,0 +1,12 @@
+import "./Coffee2.css";
+
+const Coffee2 = () => {
+  return (
+    <div className="relative sm:scale-125 xs:scale-90 scale-75">
+      <div className="cup"></div>
+      <div className="coffee"></div>
+    </div>
+  );
+};
+
+export default Coffee2;
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,8 @@ import Loader from "./Loader";
 import DarkLightButton from "./DarkLightButton";
 import LandingImageCards from "./LandingImageCards";
 import Coffee from "./Coffee";
+import Coffee2 from "./Coffee2";
+import SpiralText from "./SpiralText";
 import { NavbarBrand, MenuButton, MenuBackground, MenuLists } from "./Navbar";
 
 export {
@@ -9,6 +11,8 @@ export {
   DarkLightButton,
   LandingImageCards,
   Coffee,
+  Coffee2,
+  SpiralText,
   NavbarBrand,
   MenuButton,
   MenuLists,
