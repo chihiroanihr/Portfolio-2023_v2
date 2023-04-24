ab94a0d6f102b212b3665ec2ee34c571c1d4710d
Rhina Kim <chihiroanihr@gmail.com>
2023-04-24 19:12:20 -0400

    Update vite.config.js: @contexts path added

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,10 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "src/assets"),
       "@components": path.resolve(__dirname, "src/components"),
       "@layouts": path.resolve(__dirname, "src/layouts"),
-      "@constants": path.resolve(__dirname, "src/constants"),
       "@pages": path.resolve(__dirname, "src/pages"),
+      "@contexts": path.resolve(__dirname, "src/contexts"),
       "@utils": path.resolve(__dirname, "src/utils"),
+      "@constants": path.resolve(__dirname, "src/constants"),
     },
   },
 });
