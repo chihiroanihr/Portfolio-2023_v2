cd410afb375dd370667967e93df9403199974306
Rhina Kim <chihiroanihr@gmail.com>
2023-04-26 12:29:35 -0400

    Update About.js, Update buttonStyle.js
    - buttonStyle.js variable name changed
    
    About.jsx now implements:
    - ToggleModalProvider
    - ScrollLockProvider
    - ModalButton.jsx (Open Modal)
    - TimelineModal.jsx
    
    About.jsx text contents changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,14 @@
 import { useRef, useContext, useLayoutEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { BlobImage, BlobDots, Button, SocialIcons } from "@components";
-import { PlayAnimationContext } from "@contexts";
-import { buttonStyle } from "@constants/buttonStyle";
+import { BlobImage, BlobDots, SocialIcons, ModalButton } from "@components";
+import { TimelineModal } from "@layouts";
+import {
+  PlayAnimationContext,
+  ScrollLockProvider,
+  ToggleModalProvider,
+} from "@contexts";
+import { buttonStyle } from "@constants";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -65,66 +70,76 @@ const About = (props) => {
       className={`${classes} overflow-hidden
       lg:px-[100px] md:px-[70px] sm:px-[45px] xs:px-[35px] px-[20px]`}
     >
-      <div
-        className="relative xl:h-screen min-h-screen
-        flex flex-col-reverse xl:flex-row xl:gap-10 gap-0 justify-center items-center"
-      >
-        {/* -------- Button Column -------- */}
-        <Button
-          className={`about-view-more-btn
-          xl:hidden ${buttonStyle.aboutBtnStyle}`}
-        >
-          View More
-        </Button>
-        {/* -------- Image Area -------- */}
-        <div
-          className="xl:flex-1 relative overflow-hidden h-full w-full
-          lg:min-h-[50vh] min-h-[45vh]"
-        >
-          <BlobDots
-            className="absolute w-full h-full left-[-5%] top-[-1%]"
-            fillColor="fill-coffee-600 dark:fill-coffee-300"
-          />
-          <BlobImage className="absolute w-full h-full right-[-5%] bottom-[-1%]" />
-        </div>
-        {/* -------- Text Area -------- */}
-        <div
-          className="xl:flex-1 xs:pr-[80px] pr-[60px] max-w-screen-md flex flex-col
-          text-coffee-600 dark:text-coffee-300"
-        >
+      <ScrollLockProvider>
+        <ToggleModalProvider>
           <div
-            className="about-full-name
+            className="relative xl:h-screen min-h-screen
+        flex flex-col-reverse xl:flex-row xl:gap-10 gap-0 justify-center items-center"
+          >
+            {/* -------- Button Column (small screen) -------- */}
+            <ModalButton
+              className={`about-view-more-btn
+            xl:hidden mb-3 ${buttonStyle.aboutModalOpenBtnStyle}`}
+            >
+              View More
+            </ModalButton>
+            {/* -------- Image Area -------- */}
+            <div
+              className="xl:flex-1 relative overflow-hidden h-full w-full
+          lg:min-h-[50vh] min-h-[45vh]"
+            >
+              <BlobDots
+                className="absolute w-full h-full left-[-5%] top-[-1%]"
+                fillColor="fill-coffee-600 dark:fill-coffee-300"
+              />
+              <BlobImage className="absolute w-full h-full right-[-5%] bottom-[-1%]" />
+            </div>
+            {/* -------- Text Area -------- */}
+            <div
+              className="xl:flex-1 xs:pr-[80px] pr-[60px] max-w-screen-md flex flex-col
+          text-coffee-600 dark:text-coffee-300"
+            >
+              <div
+                className="about-full-name
             font-autography-cursive sm:text-[80px] xs:text-[58px] text-[48px] whitespace-nowrap"
-          >
-            Rhina Kim
-          </div>
-          <div className="font-default-sans lg:text-[24px] sm:text-[20px] xs:text-[16px] text-[14px] sm:leading-[1.625em] xs:leading-6 leading-5">
-            <p className="about-text-1">
-              I'm a web developer and designer with a passion for creating
-              inviting and engaging websites.
-            </p>
-            <div className="m-[25px]"></div>
-            <p className="about-text-2">
-              Just like a good cup of coffee, I believe that a website should be
-              both functional and enjoyable, providing a comfortable experience
-              for its users.
-            </p>
-          </div>
-          {/* -------- Button Row -------- */}
-          <Button
-            className={`xl:block hidden self-center mt-5 ${buttonStyle.aboutBtnStyle}`}
-          >
-            View More
-          </Button>
-        </div>
-        {/* -------- Social Icons -------- */}
-        <SocialIcons
-          className="about-social-icons
+              >
+                Rhina Kim
+              </div>
+              <div className="font-default-sans lg:text-[24px] sm:text-[20px] xs:text-[16px] text-[14px] sm:leading-[1.625em] xs:leading-6 leading-5">
+                <p className="about-text-1">
+                  I'm a web developer and designer with a passion for creating
+                  immersive digital experiences. My expertise lies in developing
+                  websites that seamlessly blend form and function, leaving a
+                  lasting impression on visitors - just like a perfect brewed
+                  cup of coffee.
+                </p>
+                <div className="m-[20px]"></div>
+                <p className="about-text-2">
+                  From the first sip of coffee to the last line of code, I pour
+                  my heart and soul into every project.
+                </p>
+              </div>
+              {/* -------- Button Row (large screen) -------- */}
+              <ModalButton
+                className={`about-view-more-btn
+              xl:block hidden self-center mt-5 ${buttonStyle.aboutModalOpenBtnStyle}`}
+              >
+                View More
+              </ModalButton>
+            </div>
+            {/* -------- Social Icons -------- */}
+            <SocialIcons
+              className="about-social-icons
           gap-2 lg:scale-[1] xs:scale-[0.8] scale-[0.7]
           absolute xl:top-1/2 xl:-translate-y-1/2
           right-0 lg:top-[18%] md:top-[15%] xs:top-[13%] top-[7%]"
-        />
-      </div>
+            />
+
+            {/* ------- Modal (hidden) ------- */}
+            <TimelineModal />
+          </div>
+        </ToggleModalProvider>
+      </ScrollLockProvider>
     </div>
   );
 };
