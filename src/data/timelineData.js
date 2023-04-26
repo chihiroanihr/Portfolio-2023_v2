2dd7c0c2cbeb820f6684f2ac3ab7266953240dc9
Rhina Kim <chihiroanihr@gmail.com>
2023-04-26 19:20:52 -0400

    Update timelineData.js: Timeline Data Added/Modified

--- src/data/timelineData.js
+++ src/data/timelineData.js
@@ -5,38 +5,39 @@ import concordiaLogoImg from "@assets/images/concordia-logo.png";
 export const workTimelineData = [
   {
     id: 0,
-    position: "Position 1 - A company",
+    position: "Front-end Developer",
+    company: "Nuance Communications Inc.",
+    type: "Internship",
     logo: nuanceLogoImg,
-    date: "Mar 1999",
-    location: "Some work experience",
+    date: "Jan 2022 — Apr 2022",
+    location: "Remote",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
-    tools: ["HTML5", "CSS3", "Javascript", "jQuery"],
+      "Built and customized reusable front-end components, contributed to the UI component library, implemented interactive React components using lifecycle methods and other features, and created a design library using Storybook to match the company's brand colors... thanks to my supportive teams and the fuel provided by caffeine ☕",
+    tools: ["React", "Javascript", "Typescript", "Storybook"],
   },
   {
     id: 1,
-    position: "Position 1 - A company",
+    position: "Mobile Application Developer",
+    company: "Sun Life Financial",
+    type: "Internship",
     logo: sunLifeLogoImg,
-    date: "Mar 1999",
-    location: "Some work experience",
+    date: "May 2021 — Aug 2021",
+    location: "Remote",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
-    tools: ["HTML5", "CSS3", "Javascript", "jQuery"],
+      'Assisted multiple POC developments, improved "my Sun Life" mobile App\'s UI/UX, experimented with Carplay support, engaged in R&D on mobile security and mobile API as a part of digital strategy, conducted Adaptive Authentication research, and ensured code reliability on "my Sun Life" mobile app using the Espresso☕ testing framework.',
+    tools: ["Java", "Kotlin", "Swift", "Android SDK", "Espresso"],
   },
   {
     id: 2,
-    position: "Position 1 - A company",
+    position: "Android Application Developer",
+    company: "Sun Life Financial",
+    type: "Internship",
     logo: sunLifeLogoImg,
-    location: "Some work experience",
+    date: "Sep 2020 — Dec 2020",
+    location: "Remote",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
-  },
-  {
-    id: 3,
-    position: "Position 1 - A company",
-    location: "Some work experience",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+      'First internship. Worked with the production team to build new UI features on the "Lumino Health" mobile application, led two POC developments on the "my Sun Life" mobile application, and performed UI testing using the Espresso testing framework.',
+    tools: ["Java", "Kotlin", "Android SDK", "Espresso"],
   },
 ];
 
@@ -45,36 +46,41 @@ export const lifeTimelineData = [
     id: 0,
     date: "Mar 1999",
     title: ". . . Hailing from SOUTH KOREA 🕊️",
+    location: "Gwangju",
+    description: "Never have to worry about running out of spicy food.",
   },
   {
     id: 1,
     date: "Jul 1999",
-    title: "Move to JAPAN 🗼",
+    title: "Migrate to JAPAN 🗼",
     location: "Tokyo",
+    description:
+      "Public transportations are too punctual that you really cannot make up an excuse for being late.",
   },
   {
     id: 2,
     date: "Jul 2015",
-    title: "Move to CANADA 🍁",
+    title: "🍁 Study Abroad to CANADA",
     location: "Montreal, QC",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Came to improve my English skills...\nAttends, why is everything in French?",
   },
   {
     id: 3,
     date: "Sep 2019",
-    title: "Begins Studies @Concordia University",
+    title: "Begins Studies @ Concordia University",
     logo: concordiaLogoImg,
     location: "Montreal, QC",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+      "Object Oriented Programming, System Hardware, Operation System, Data Structure & Algorithms, Computer Networks, Artificial Intelligence, Web Programming, etc...\
+      \nHome of the endless coding marathons and the land of the 2 a.m. caffeine runs :)",
   },
   {
     id: 4,
     date: "Dec 2022",
-    title: "Graduates Concordia University - Bachelor of Computer Science 🎓",
+    title:
+      "🎓 Graduates Concordia University — Bachelor of Computer Science (co-op)",
     location: "Montreal, QC",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+    description: "YEEHAW",
   },
 ];
