f0805e118d233acd88e457bdb736d79c617a177f
Rhina Kim <chihiroanihr@gmail.com>
2023-04-26 11:57:12 -0400

    timelineData.js added to /src/data/

--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,12 @@
 import imageCardsData from "./imageCardsData";
 import menuListsData from "./menuListsData";
 import socialMediaPlatformsData from "./socialMediaPlatformsData";
+import { workTimelineData, lifeTimelineData } from "./timelineData";
 
 export {
   imageCardsData,
   menuListsData,
   socialMediaPlatformsData,
+  workTimelineData,
+  lifeTimelineData,
 };
---
+++ src/data/timelineData.js
@@ -0,0 +1,80 @@
+import sunLifeLogoImg from "@assets/images/sun-life-logo.png";
+import nuanceLogoImg from "@assets/images/nuance-logo.png";
+import concordiaLogoImg from "@assets/images/concordia-logo.png";
+
+export const workTimelineData = [
+  {
+    id: 0,
+    position: "Position 1 - A company",
+    logo: nuanceLogoImg,
+    date: "Mar 1999",
+    location: "Some work experience",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+    tools: ["HTML5", "CSS3", "Javascript", "jQuery"],
+  },
+  {
+    id: 1,
+    position: "Position 1 - A company",
+    logo: sunLifeLogoImg,
+    date: "Mar 1999",
+    location: "Some work experience",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+    tools: ["HTML5", "CSS3", "Javascript", "jQuery"],
+  },
+  {
+    id: 2,
+    position: "Position 1 - A company",
+    logo: sunLifeLogoImg,
+    location: "Some work experience",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+  },
+  {
+    id: 3,
+    position: "Position 1 - A company",
+    location: "Some work experience",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+  },
+];
+
+export const lifeTimelineData = [
+  {
+    id: 0,
+    date: "Mar 1999",
+    title: ". . . Hailing from SOUTH KOREA 🕊️",
+  },
+  {
+    id: 1,
+    date: "Jul 1999",
+    title: "Move to JAPAN 🗼",
+    location: "Tokyo",
+  },
+  {
+    id: 2,
+    date: "Jul 2015",
+    title: "Move to CANADA 🍁",
+    location: "Montreal, QC",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 3,
+    date: "Sep 2019",
+    title: "Begins Studies @Concordia University",
+    logo: concordiaLogoImg,
+    location: "Montreal, QC",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+  },
+  {
+    id: 4,
+    date: "Dec 2022",
+    title: "Graduates Concordia University - Bachelor of Computer Science 🎓",
+    location: "Montreal, QC",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+  },
+];
