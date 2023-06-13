fe731c22738dc00a5ee885801b2510a3354958e9
Rhina Kim <chihiroanihr@gmail.com>
2023-06-12 20:20:56 -0400

    @components/SVG/TwoCoffeeCups.jsx deleted

--- src/components/SVG/index.js
+++ src/components/SVG/index.js
@@ -1,6 +1,5 @@
 import Signature from "./Signature";
-import TwoCoffeeCups from "./TwoCoffeeCups";
 import IdeaLightBulb from "./IdeaLightBulb";
 import CoffeeCupLine from "./CoffeeCupLine";
 
-export { Signature, TwoCoffeeCups, IdeaLightBulb, CoffeeCupLine };
+export { Signature, IdeaLightBulb, CoffeeCupLine };
