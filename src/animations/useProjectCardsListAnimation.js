1fc4186fe612ce8de51da5909684b31653d36cf1
Rhina Kim <chihiroanihr@gmail.com>
2023-06-14 00:36:29 -0400

    Organized animation code inside ProjectCardsListAnimation.js
    - some animation attributes changed such as animation timings, duration, etc.

--- src/animations/useProjectCardsListAnimation.js
+++ src/animations/useProjectCardsListAnimation.js
@@ -4,20 +4,71 @@ import { getOpacity, getRotation } from "@utils";
 
 gsap.registerPlugin(ScrollTrigger);
 
-function useProjectCardsListAnimation(cardsContainerNode, sectionWrapperNode) {
-  return gsap.context(() => {
-    // ------------------ Cards Container Animation ------------------ //
-    // Initialize entire cards
-    gsap.set(cardsContainerNode, { opacity: 1, autoAlpha: 1, y: 0 });
+function animateProjectCard(cardNode) {
+  // Initialize each cards
+  gsap.set(cardNode, { opacity: 0, autoAlpha: 0, y: -100 });
 
-    // Animate entire cards onLeave and onLeaveBack
-    gsap.to(cardsContainerNode, {
+  // Animate each card
+  gsap.to(cardNode, {
+    opacity: 1,
+    autoAlpha: 1,
+    y: 0,
+    duration: 1.5,
+    scrollTrigger: {
+      id: "project-card-animation-on-scroll",
+      trigger: cardNode,
+      start: "top-=90% top",
+      clearProps: true,
+      invalidateOnRefresh: true,
+    },
+  });
+}
+
+function animateProjectCardNewTag(newTagNode, cardNode) {
+  // Get current new tag styles
+  const opacityValue = getOpacity(newTagNode);
+  const rotateValue = getRotation(newTagNode, { negative: true });
+
+  // Initial new tag
+  gsap.set("#works #new-tag", {
+    opacity: 0,
+    autoAlpha: 0,
+    rotate: rotateValue - 50,
+    y: -50,
+  });
+
+  // Animate new tag
+  gsap.to("#works #new-tag", {
+    opacity: opacityValue, // animate to original opacity value
+    autoAlpha: opacityValue,
+    rotate: rotateValue, // animate to original rotation value
+    y: 0,
+    duration: 1.5,
+    ease: "elastic",
+    scrollTrigger: {
+      id: "project-card-newtag-animation-on-scroll",
+      trigger: cardNode,
+      start: "top-=90% top",
+      toggleActions: "play none restart none",
+      //once: true,
+      clearProps: true,
+      invalidateOnRefresh: true,
+    },
+  });
+}
+
+function animateProjectCardsList(cardsContainerNode, sectionWrapperNode) {
+  // Animate entire cards onLeave and onLeaveBack
+  gsap.fromTo(
+    cardsContainerNode,
+    { opacity: 1, autoAlpha: 1, y: 0 },
+    {
       opacity: 0,
       autoAlpha: 0,
       y: -100,
-      duration: 1,
+      duration: 0.8,
       scrollTrigger: {
-        id: "project-cards-list-scroll-animation",
+        id: "project-cards-list-animation-on-scroll",
         trigger: sectionWrapperNode,
         start: "top top",
         end: "bottom bottom",
@@ -25,64 +76,27 @@ function useProjectCardsListAnimation(cardsContainerNode, sectionWrapperNode) {
         clearProps: true,
         invalidateOnRefresh: true,
       },
-    });
+    }
+  );
+}
 
-    // --------------------- Each Card Animation --------------------- //
-    cardsContainerNode.childNodes.forEach((child) => {
-      // Initialize each cards
-      gsap.set(child, { opacity: 0, autoAlpha: 0, y: -100 });
+function useProjectCardsListAnimation(cardsContainerNode, sectionWrapperNode) {
+  console.log("[Render] [animation] useProjectCardsListAnimation.js");
 
+  return gsap.context(() => {
+    // ---------- Cards Container Animation ---------- //
+    animateProjectCardsList(cardsContainerNode, sectionWrapperNode);
+
+    // ------------- Each Card Animation ------------- //
+    cardsContainerNode.childNodes.forEach((cardNode) => {
       // Animate each card
-      gsap.to(child, {
-        opacity: 1,
-        autoAlpha: 1,
-        y: 0,
-        duration: 1.5,
-        scrollTrigger: {
-          id: "project-cards-list-item-card-animation",
-          trigger: child,
-          start: "top-=90% top",
-          once: true,
-          clearProps: true,
-          invalidateOnRefresh: true,
-        },
-      });
+      animateProjectCard(cardNode);
 
       // Get new tag node for each card
-      const newTagNode = child.querySelector("#new-tag");
+      const newTagNode = cardNode.querySelector("#new-tag");
 
-      if (newTagNode) {
-        // Get current new tag styles
-        const opacityValue = getOpacity(newTagNode);
-        const rotateValue = getRotation(newTagNode, { negative: true });
-
-        // Initial new tag
-        gsap.set("#works #new-tag", {
-          opacity: 0,
-          autoAlpha: 0,
-          rotate: rotateValue - 50,
-          y: -50,
-        });
-
-        // Animate new tag
-        gsap.to("#works #new-tag", {
-          opacity: opacityValue, // animate to original opacity value
-          autoAlpha: opacityValue,
-          rotate: rotateValue, // animate to original rotation value
-          y: 0,
-          duration: 1.5,
-          ease: "elastic",
-          scrollTrigger: {
-            id: "project-cards-list-item-newtag-animation",
-            trigger: child,
-            start: "top-=90% top",
-            toggleActions: "play none restart none",
-            //once: true,
-            clearProps: true,
-            invalidateOnRefresh: true,
-          },
-        });
-      }
+      // Animate new tags inside each card
+      if (newTagNode) animateProjectCardNewTag(newTagNode, cardNode);
     }),
       // Scope
       cardsContainerNode;
