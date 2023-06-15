cc0b87e2ac466eaebcb2b717979ae2c3cefb01ac
Rhina Kim <chihiroanihr@gmail.com>
2023-06-14 23:02:34 -0400

    Update vite.config.js: negligible change

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@animations": path.resolve(__dirname, "src/animations"),
       "@assets": path.resolve(__dirname, "src/assets"),
+      "@animations": path.resolve(__dirname, "src/animations"),
       "@components": path.resolve(__dirname, "src/components"),
       "@layouts": path.resolve(__dirname, "src/layouts"),
       "@views": path.resolve(__dirname, "src/views"),
