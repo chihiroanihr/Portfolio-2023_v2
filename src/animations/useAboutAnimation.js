5f32bf6fc8aa53396f8cf67f9c0685cd9bbbe05c
Rhina Kim <chihiroanihr@gmail.com>
2023-06-21 01:14:35 -0400

    Update useAboutAnimation.js: added view-more-btn pointer-events animation & some comments

--- src/animations/useAboutAnimation.js
+++ src/animations/useAboutAnimation.js
@@ -1,4 +1,4 @@
-import { gsap } from "gsap";
+import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { positionStyle } from "@themes";
 
@@ -9,21 +9,26 @@ function useAboutAnimation(sectionWrapperNode) {
 
   return gsap.context(
     () => {
-      // Initialize
+      // ------------------------- Initialize ------------------------- //
       gsap.set("#about #full-name", { y: 100, opacity: 0 });
       gsap.set("#about #local-time", { x: 100, opacity: 0 });
       gsap.set("#about #text", { y: 100, opacity: 0 });
       gsap.set("#about #images", { opacity: 0 });
-      gsap.set("#about #view-more-btn", { y: 100, opacity: 0 });
+      gsap.set("#about #view-more-btn", {
+        y: 100,
+        opacity: 0,
+        pointerEvents: "none",
+      });
       gsap.set("#about #social-icons", { y: 100, opacity: 0 });
 
       gsap.set(sectionWrapperNode, { autoAlpha: 0, opacity: 0 });
 
-      // Toggle opacity when moving between sections
+      // -------- Toggle opacity when moving between sections  -------- //
       gsap.to(sectionWrapperNode, {
-        duration: 0.5,
         autoAlpha: 1,
         opacity: 1,
+        duration: 0.5,
+        ease: "power2.out",
         clearProps: true,
         scrollTrigger: {
           id: "about-section-animation-on-scroll",
@@ -31,12 +36,25 @@ function useAboutAnimation(sectionWrapperNode) {
           start: "top-=1000 top",
           end: positionStyle.aboutToWorkSectionTransitionPosition + " top",
           toggleActions: "play reverse play none",
-          ease: "power2.out",
           invalidateOnRefresh: true, // handles change in trigger position when viewport changes
         },
       });
 
-      // About section content animation
+      // ----------- Toggle pointer events of view-more-btn ----------- //
+      const viewMoreButtonNode = document.querySelector(
+        "#about #view-more-btn"
+      );
+      ScrollTrigger.create({
+        trigger: sectionWrapperNode,
+        start: "top-=1000 top",
+        end: positionStyle.aboutToWorkSectionTransitionPosition + " top",
+        onToggle: ({ isActive }) => {
+          if (isActive) viewMoreButtonNode.style.pointerEvents = "auto";
+          else viewMoreButtonNode.style.pointerEvents = "none";
+        },
+      });
+
+      // -------------- About section content animation --------------- //
       gsap
         .timeline({
           defaults: {
